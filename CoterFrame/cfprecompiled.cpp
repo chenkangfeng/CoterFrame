9eabe18ab00d8d3bc9c907947c73132ff87a9b56
Coter <593408166@qq.com>
2016-12-17 17:40:39 +0800

    add

---
+++ CoterFrame/cfprecompiled.cpp
@@ -0,0 +1,2 @@
+
+#include "cfprecompiled.h"
---
+++ CoterFrame/cfprecompiled.h
@@ -0,0 +1,15 @@
+
+#ifndef CF_PRECOMPILED_H
+#define CF_PRECOMPILED_H
+
+#include <string>
+#include "include/cfplatform.h"
+#include "include/cfmacro.h"
+#include "kernel/cfsingleton.hpp"
+#include "kernel/cfsharedptr.hpp"
+#include "kernel/cfobject.h"
+#include "kernel/cfglobal.hpp"
+#include "kernel/cfinterface.hpp"
+#include "kernel/cfcomponent.hpp"
+
+#endif // CF_PRECOMPILED_H
