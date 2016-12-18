cfbebbe534af43a288a7982e6686c4c5608cd8f7
Coter <593408166@qq.com>
2016-12-18 23:14:39 +0800

    .

---
+++ CoterFrame/kernel/cfstl.hpp
@@ -0,0 +1,31 @@
+
+#ifndef CF_STL_HPP
+#define CF_STL_HPP
+
+#include <list>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include "include/cfmacro.h"
+#include "cfsharedptr.hpp"
+
+NS_CF_BEGIN
+
+template <typename T>
+class CFList : public std::list<CF_SHARED_PTR<T> > {};
+
+template <typename T>
+class CFVector : public std::vector<CF_SHARED_PTR<T> > {};
+
+template <typename Key, typename Value, CFBool ordered = false>
+class CFMap {};
+
+template <typename Key, typename Value>
+class CFMap<Key, Value, false> : public std::unordered_map<Key, CF_SHARED_PTR<Value> > {};
+
+template <typename Key, typename Value>
+class CFMap<Key, Value, true> : public std::map<Key, CF_SHARED_PTR<Value> > {};
+
+NS_CF_END
+
+#endif // CF_STL_HPP
